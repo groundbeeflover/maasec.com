2d1f600aa7f1ce6aeb3b690c12aa53bafd925dfa
Dimi <johnnnathan@proton.me>
2025-02-16 13:44:09 +0100

    Config change

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   basePath: "/maasec.com",
   output: 'export',
   reactStrictMode: true,
-  path: "/",
+  assetPrefix: "/maasec.com/"
 };
 
 module.exports = nextConfig;

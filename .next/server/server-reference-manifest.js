ab0a94ac8ce30b7ec10da57024a97ef23ab2e44e
Dimi <johnnnathan@proton.me>
2025-02-16 10:54:10 +0100

    Added static export

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YuxNxnryqxbPjJ7rugzTwXRLYCkHius4grwi0idwJ48=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"

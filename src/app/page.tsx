4ab6a4c1c0061fa64fc0a887acf4c7458c97942e
Dimi <johnnnathan@proton.me>
2025-02-16 12:16:48 +0100

    img to Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -188,10 +188,10 @@ export default function Home() {
             <Github className="w-10 h-10 inline text-black" />
           </a>
           <a href="https://discord.com/invite/ghpWe7wawV" className="text-white hover:bg-gray-200 transition-colors">
-            <img src="./images/discord.svg" className="w-10 h-10 inline" />
+            <Image src="./images/discord.svg" className="w-10 h-10 inline" />
           </a>
           <a href="https://ctftime.org/team/365940" className="text-white hover:bg-gray-200 transition-colors">
-            <img src="./images/ctftime.png" className="w-10 h-10 inline" />
+            <Image src="./images/ctftime.png" className="w-10 h-10 inline" />
           </a>
         </div>
       </section>

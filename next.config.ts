df1d6d115e6f24547d3487af4e6e8462c429c553
Dimi <johnnnathan@proton.me>
2025-02-16 11:38:20 +0100

    Set-up for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/maasec.com",
   output: 'export',
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;

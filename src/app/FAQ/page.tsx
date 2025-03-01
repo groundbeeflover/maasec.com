dd9279455fb1f9583bfef3953e765f9aeaa325fe
groundbeeflover <gevegg22@gmail.com>
2025-03-01 18:14:49 +0100

    initial commit i forgot to do this earlier

---
+++ src/app/FAQ/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+import {Body, H1} from "../design-system/formatting"
+
+const FAQ = () => {
+    return (
+        <Body>
+            <H1>FAQ</H1>
+        </Body>
+    )
+}
+export default FAQ
